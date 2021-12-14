01a3243d5cf65952ccea481a931ca75b760be5b4
Hualiang Yang <yanghualiang@minieye.cc>
2021-12-14 17:35:03 +0800

    add test virtual memory function

---
+++ vm/test_virtual.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main()
+{
+    char *p = (char *)malloc(20);
+    strcpy(p, "123");
+    pid_t pid = getpid();
+    printf("pid: %d\n", pid);
+
+    pause();
+
+    return 0;
+}
